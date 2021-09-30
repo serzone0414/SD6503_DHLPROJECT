6a28f11c196ce14d4c2c9e0eadb1fffc852f6b5a
Long Shi <serzone_0414@hotmail.com>
2021-10-01 00:03:01 +1300

    Second sprint

--- SD6503_DHLPROJECT/Controllers/HomeController.cs
+++ SD6503_DHLPROJECT/Controllers/HomeController.cs
@@ -2,12 +2,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SD6503_DHLPROJECT.Models;
+using SD6503_DHLPROJECT.Controllers;
 using System;
 using System.Collections.Generic;
 using System.Collections;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace SD6503_DHLPROJECT.Controllers
 {
@@ -51,10 +54,18 @@ namespace SD6503_DHLPROJECT.Controllers
                 _context.LoginAccounts.Add(user);
                 _context.SaveChanges();
 
+                LoginAccount loginAccount = _context.LoginAccounts.Where(u => u.Username == user.Username).FirstOrDefault();
+                AccountDetail accountDetail = new AccountDetail();
+                accountDetail.Identifier = loginAccount.Identifier;
+                accountDetail.Name = "New User";
+                accountDetail.Balance = 0;
+                _context.AccountDetails.Add(accountDetail);
+                _context.SaveChanges();
+
                 ModelState.Clear();
                 ViewBag.Message = user.Username + " is successfully registered.";
             }
-            return View();
+            return RedirectToAction("Index");
         }
 
         public ActionResult Login()
@@ -82,13 +93,65 @@ namespace SD6503_DHLPROJECT.Controllers
         }
 
 
-        public ActionResult LoggedIn()
+        public ActionResult LoggedIn(string searchBy, string search)
         {
             if (HttpContext.Session.GetString("Identifier") != null)
             {
+                //Get Account details
+                var accountDetail = _context.AccountDetails.Where(u => u.Identifier == Int32.Parse(HttpContext.Session.GetString("Identifier"))).FirstOrDefault();
+                HttpContext.Session.SetString("Username", accountDetail.Name);
+                HttpContext.Session.SetString("Balance", accountDetail.Balance.ToString());
+                HttpContext.Session.SetString("AccountNumber", accountDetail.AccountNumber.ToString());
+                if (accountDetail.Name == "New User")
+                {
+                    return RedirectToAction("NewUser");
+                }
                 ViewBag.Username = HttpContext.Session.GetString("Username");
-                ViewBag.IsAdmin = HttpContext.Session.GetString("IsAdmin");
-                return View();
+                ViewBag.Balance = HttpContext.Session.GetString("Balance");
+                ViewBag.Identifier = accountDetail.AccountNumber.ToString();
+
+                List<AccountDetail> accountsList = new List<AccountDetail>();
+                accountsList = _context.AccountDetails.ToList();
+                ViewBag.accountsList = accountsList;
+
+                //Get Transaction details
+                if (search == null)
+                {
+                    IList<TransactionTable> relatedTransactionList = new List<TransactionTable>();
+                    return View(_context.TransactionTables.Where(u => u.FromAccount == accountDetail.AccountNumber || u.ToAccount == accountDetail.AccountNumber).ToList());
+                }
+                else
+                {
+                    if (searchBy == "Name")
+                    {
+                        var searchingAccount = _context.AccountDetails.Where(u => u.Name.StartsWith(search)).FirstOrDefault();
+                        if (searchingAccount != null)
+                        {
+                            int searchingAccountNumber = searchingAccount.AccountNumber;
+                            IList<TransactionTable> relatedTransactionList = new List<TransactionTable>();
+                            var returningList = _context.TransactionTables.Where(u => u.FromAccount == accountDetail.AccountNumber || u.ToAccount == accountDetail.AccountNumber).Where(u => u.FromAccount == searchingAccountNumber || u.ToAccount == searchingAccountNumber).ToList();
+                            return View(returningList);
+                        }
+                        else
+                            return View(_context.TransactionTables.Where(u => u.FromAccount == 000000).ToList());
+                    }
+                    else 
+                    {
+                        int searchingAccountNumber;
+                        bool isNumber = int.TryParse(search, out searchingAccountNumber);
+                        if (isNumber)
+                        {
+                            IList<TransactionTable> relatedTransactionList = new List<TransactionTable>();
+                            var returningList = _context.TransactionTables.Where(u => u.FromAccount == accountDetail.AccountNumber || u.ToAccount == accountDetail.AccountNumber).Where(u => u.FromAccount == searchingAccountNumber || u.ToAccount == searchingAccountNumber).ToList();
+                            return View(returningList);
+                        }
+                        else
+                        {
+                            return View(_context.TransactionTables.Where(u => u.FromAccount == 000000).ToList());
+                        }
+
+                    }
+                } 
             }
             else
             {
@@ -100,6 +163,122 @@ namespace SD6503_DHLPROJECT.Controllers
         {
             HttpContext.Session.Clear();
             return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Lend()
+        {
+            if (HttpContext.Session.GetString("AccountNumber") != null)
+            {
+                var accountDetail = _context.AccountDetails.Where(u => u.Identifier == Int32.Parse(HttpContext.Session.GetString("Identifier"))).FirstOrDefault();
+                ViewData["FromAccount"] = new SelectList(_context.AccountDetails.Where(u => u.AccountNumber == accountDetail.AccountNumber), "AccountNumber", "Name");
+                return View();
+            }
+            else
+                return RedirectToAction("Index");
+
+        }
+
+        // POST: Home/Lend
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Lend([Bind("TransactionNumber,ToAccount,FromAccount,LendAmount,0")] TransactionTable transactionTable)
+        {
+            if (ModelState.IsValid)
+            {
+                var fromAccount = _context.Set<AccountDetail>().SingleOrDefault(o => o.AccountNumber == transactionTable.FromAccount);
+                fromAccount.Balance = fromAccount.Balance - transactionTable.LendAmount;
+                var toAccount = _context.Set<AccountDetail>().SingleOrDefault(o => o.AccountNumber == transactionTable.ToAccount);
+                toAccount.Balance = toAccount.Balance + transactionTable.LendAmount;
+
+                var existTransaction = _context.Set<TransactionTable>().Where(o => o.FromAccount == transactionTable.FromAccount && o.ToAccount == transactionTable.ToAccount).FirstOrDefault();
+                //var existTransaction = _context.Set<TransactionTable>().SingleOrDefault(o => o.FromAccount == transactionTable.FromAccount && o.ToAccount == transactionTable.ToAccount);
+                if (existTransaction != null)
+                {
+                    existTransaction.LendAmount = existTransaction.LendAmount + transactionTable.LendAmount;
+                }
+                else
+                {
+                    _context.Add(transactionTable);
+                }
+                await _context.SaveChangesAsync();
+                return RedirectToAction("LoggedIn");
+            }
+            ViewData["FromAccount"] = new SelectList(_context.AccountDetails, "AccountNumber", "Name", transactionTable.FromAccount);
+            return View(transactionTable);
+        }
+
+        public IActionResult PayBack()
+        {
+            if (HttpContext.Session.GetString("AccountNumber") != null)
+            {
+                var fromAccountDetail = _context.AccountDetails.Where(u => u.Identifier == Int32.Parse(HttpContext.Session.GetString("Identifier"))).FirstOrDefault();
+                ViewData["FromAccount"] = new SelectList(_context.AccountDetails.Where(u => u.AccountNumber == fromAccountDetail.AccountNumber), "AccountNumber", "Name");
+
+
+                List<TransactionTable> ToAccountTransactionList = _context.TransactionTables.Where(u => u.ToAccount == fromAccountDetail.AccountNumber ).ToList();
+                List<AccountDetail> relatedAccountList = new List<AccountDetail>();
+                foreach (var transaction in ToAccountTransactionList)
+                {
+                    var realtedAccountNumber = _context.AccountDetails.Where(u => u.AccountNumber == transaction.FromAccount).FirstOrDefault();
+                    if (realtedAccountNumber != null)
+                    {
+                        relatedAccountList.Add(realtedAccountNumber);
+                    }
+                }
+                ViewData["ToAccount"] = new SelectList(relatedAccountList, "AccountNumber", "Name");
+
+
+                return View();
+            }
+            else
+                return RedirectToAction("Index");
+        }
+
+        // POST: Home/PayBack
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> PayBack([Bind("TransactionNumber,ToAccount,FromAccount,0,PaybackAmount")] TransactionTable transactionTable)
+        {
+            if (ModelState.IsValid)
+            {
+                var fromAccount = _context.Set<AccountDetail>().SingleOrDefault(o => o.AccountNumber == transactionTable.FromAccount);
+                fromAccount.Balance = fromAccount.Balance - transactionTable.PaybackAmount;
+                var toAccount = _context.Set<AccountDetail>().SingleOrDefault(o => o.AccountNumber == transactionTable.ToAccount);
+                toAccount.Balance = toAccount.Balance + transactionTable.PaybackAmount;
+
+                var existTransaction = _context.Set<TransactionTable>().Where(o => o.ToAccount == transactionTable.FromAccount && o.FromAccount == transactionTable.ToAccount).FirstOrDefault();
+                //var existTransaction = _context.Set<TransactionTable>().SingleOrDefault(o => o.FromAccount == transactionTable.FromAccount && o.ToAccount == transactionTable.ToAccount);
+                if (existTransaction != null)
+                {
+                    existTransaction.PaybackAmount = existTransaction.PaybackAmount + transactionTable.PaybackAmount;
+                }
+                else
+                {
+                    _context.Add(transactionTable);
+                }
+                await _context.SaveChangesAsync();
+                return RedirectToAction("LoggedIn");
+            }
+            ViewData["FromAccount"] = new SelectList(_context.AccountDetails, "AccountNumber", "Name", transactionTable.FromAccount);
+            return View(transactionTable);
+        }
+
+        public IActionResult NewUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewUser(string name)
+        {
+            AccountDetail accountDetail = _context.AccountDetails.Where(u => u.AccountNumber == Int32.Parse(HttpContext.Session.GetString("AccountNumber"))).FirstOrDefault();
+            accountDetail.Name = name;
+            _context.Update(accountDetail);
+            _context.SaveChanges();
+
+            return RedirectToAction("LoggedIn");
         }
     }
 }
---
+++ SD6503_DHLPROJECT/Models/NewRegister.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SD6503_DHLPROJECT.Models
+{
+    public class NewRegister
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Name { get; set; }
+    }
+}
