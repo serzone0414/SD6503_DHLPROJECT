c02b7fcdbbce0f7d5f6e8f890d18ceed357139be
Hamish Baird <Hamish.Baird01@student.weltec.ac.nz>
2021-10-07 18:57:56 +1300

    testing started

---
+++ SD6503_DHLPROJECT/Controllers/AccountDetailsTestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SD6503_DHLPROJECT.Controllers
+{
+    public class AccountDetailsTestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ UnitTestProject1/AutomatedUi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class AutomatedUi
+{
+        private readonly IWebDriver _driver;
+        public AutomatedUi()
+        {
+            _driver = new ChromeDriver();
+        }
+
+    [TestMethod]
+
+    public void launchbrowser()
+        {
+            _driver.Navigate().GoToUrl("https://localhost:5001/");
+            Assert.AreEqual("Index - SD6503_DHLPROJECT", _driver.Title);
+        }
+}
+
+
+
+}
